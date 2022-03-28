ee7b32b63d409bedb536919b75a7cbf518e4e0b7
komplektan <54449303+komplektan@users.noreply.github.com>
2022-03-28 19:03:27 +0500

    Initial Commit

---
+++ WhatToCook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToCook
+//
+//  Created by Вячеслав Квашнин on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
