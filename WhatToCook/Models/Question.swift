ea0998e9dc603e4328b5af9c9d779c4050153446
komplektan <54449303+komplektan@users.noreply.github.com>
2022-03-29 19:58:45 +0500

    Create Model and Services

---
+++ WhatToCook/Models/Question.swift
@@ -0,0 +1,70 @@
+//
+//  Question.swift
+//  WhatToCook
+//
+//  Created by Вячеслав Квашнин on 29.03.2022.
+//
+
+import Foundation
+
+
+struct Question {
+    let title: String
+    let type: IngredientType
+    let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Жидкости",
+                type: .liquid,
+                answers: [
+                    Answer(title: "Вода"),
+                    Answer(title: "Молоко"),
+                    Answer(title: "Квас"),
+                    Answer(title: "Kефир")
+                ]),
+            Question(
+                title: "Сыпучие продукты",
+                type: .bulkProducts,
+                answers: [
+                    Answer(title: "Гречка"),
+                    Answer(title: "Рис"),
+                    Answer(title: "Горох"),
+                    Answer(title: "Перловка"),
+                    Answer(title: "Макароны")
+                ]),
+            Question(
+                title: "Овощи",
+                type: .vegetables,
+                answers: [
+                    Answer(title: "Картофель"),
+                    Answer(title: "Капуста"),
+                    Answer(title: "Свекла"),
+                    Answer(title: "Помидоры"),
+                    Answer(title: "Морковь")
+                ]),
+            Question(
+                title: "Мясо",
+                type: .meat,
+                answers: [
+                    Answer(title: "Курица"),
+                    Answer(title: "Говядина"),
+                    Answer(title: "Свинина"),
+                    Answer(title: "Баранина"),
+                    Answer(title: "Колбаса")
+                ])
+        ]
+    }
+}
+
+enum IngredientType {
+    case liquid
+    case bulkProducts
+    case vegetables
+    case meat
+}
+
+struct Answer {
+    let title: String
+}
---
+++ WhatToCook/Services/DishesManager.swift
@@ -0,0 +1,15 @@
+//
+//  Dishes.swift
+//  WhatToCook
+//
+//  Created by Вячеслав Квашнин on 29.03.2022.
+//
+
+import Foundation
+
+class DishesManager {
+    
+    let dishes = [
+    "Куриный суп с яйцом и зеленью"
+    ]
+}
